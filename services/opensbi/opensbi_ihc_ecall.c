d077a629418d5c6cab1fca31d84ff801a8983825
Tero Salminen <tero.salminen@unikie.com>
2021-12-22 09:34:20 +0200

    IHC RX: copy full message content to destination buffer
    Signed-off-by: Tero Salminen <tero.salminen@unikie.com>
    Change-Id: I1913ac1a27665e22f6e1cc49a0e5a01a92ad096f

--- services/opensbi/opensbi_ihc_ecall.c
+++ services/opensbi/opensbi_ihc_ecall.c
@@ -43,7 +43,8 @@ static uint32_t message_present_handler(uint32_t remote_hart_id, uint32_t * mess
         msg.irq_type = ACK_IRQ;
     } else {
         msg.irq_type = MP_IRQ;
-        memcpy((uint32_t *) &msg.ihc_msg, message, message_size);
+        /* message_size is number of uint32_t elements */
+        memcpy((uint32_t *) &msg.ihc_msg, message, message_size * (sizeof(uint32_t)));
     }
 
     memcpy((uint32_t *) message_storage_ptr, (uint32_t *) &msg, sizeof(struct ihc_sbi_rx_msg));
